0622bbe30e3e06893e5efc352b7dc61b660c65be
jlareo <lareomerlin@gmail.com>
2013-04-27 18:26:01 +0100

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,13 @@
 include ApplicationHelper
+
+def valid_signin(user) #example:   before { valid_signin(user) }
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message| #example: it { should have_error_message('Invalid') }
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
